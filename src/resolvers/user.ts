3318abad6383a20be4f60c614c26837eb3c43cfe
qxl0 <qxl0@yahoo.com>
2021-07-04 13:31:54 -0400

    added support for login

--- src/resolvers/user.ts
+++ src/resolvers/user.ts
@@ -1,6 +1,6 @@
 import { User } from "../entities/User";
 import { MyContext } from "src/types";
-import { Arg, Ctx, Field, InputType, Mutation, Query, Resolver } from "type-graphql";
+import { Arg, Ctx, Field, InputType, Mutation, ObjectType, Query, Resolver } from "type-graphql";
 import argon2 from 'argon2';
 
 @InputType()
@@ -9,6 +9,22 @@ class UsernamePassordInput {
     username: string
     @Field()
     password: string
+}
+
+@ObjectType()
+class FieldError {
+    @Field()
+    field: string;
+    @Field()
+    message: string;
+}
+@ObjectType()
+class UserResponse {
+    @Field(() => [FieldError], {nullable: true})
+    errors?:  FieldError[]
+
+    @Field()
+    user?: User
 }
 
 
@@ -26,5 +42,38 @@ export class UserResolver {
             });
         await em.persistAndFlush(user);
         return user;
+    }
+
+
+    @Mutation(() => UserResponse)
+    async login(
+        @Arg('options') options: UsernamePassordInput,
+        @Ctx() {em}: MyContext
+    ): Promise<UserResponse> {
+        const user = await em.findOne(User, {username: options.username});
+        if (!user) {
+            return {
+                errors: [
+                    {
+                    field: 'username',
+                    message: "user name doesn''t exist "
+                    }
+            ],
+            }
+        };
+        const valid = await argon2.verify(user.password, options.password);
+        if (!valid){
+            return {
+                errors: [
+                    {
+                    field: 'password',
+                    message: "incorrect password"
+                    }
+                ],
+            };
+        }
+        return {
+            user,
+        };
     }
 }
