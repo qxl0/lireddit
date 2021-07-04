ff39b0b7a2ec2ac62b1069420ba3141f38c04518
qxl0 <qxl0@yahoo.com>
2021-07-04 16:22:34 -0400

    added cookie support so user can stay log on

--- src/resolvers/user.ts
+++ src/resolvers/user.ts
@@ -30,10 +30,21 @@ class UserResponse {
 
 @Resolver()
 export class UserResolver {
+    @Query(() => User, {nullable: true})
+    async me(
+        @Ctx() { req, em }: MyContext
+    ){
+        if (!req.session.userId){
+            return null;
+        }
+
+        const user = await em.findOne(User, { id: req.session.userId });
+        return user;
+    }
     @Mutation(() => UserResponse)
     async register(
         @Arg('options') options: UsernamePassordInput,
-        @Ctx() {em}: MyContext
+        @Ctx() {em, req}: MyContext
     ): Promise<UserResponse> {
 
         if (options.username.length <= 2){
@@ -80,6 +91,12 @@ export class UserResolver {
             console.log("error message is: ", err.message);
 
         }
+
+        // store user id session
+        // this will set a cookie on the user
+        // keep them logged in
+        req.session.userId = user.id;
+
         return {user};
     }
 
@@ -87,7 +104,7 @@ export class UserResolver {
     @Mutation(() => UserResponse)
     async login(
         @Arg('options') options: UsernamePassordInput,
-        @Ctx() {em}: MyContext
+        @Ctx() {em, req}: MyContext
     ): Promise<UserResponse> {
         const user = await em.findOne(User, {username: options.username});
         if (!user) {
@@ -111,6 +128,9 @@ export class UserResolver {
                 ],
             };
         }
+
+        req.session.userId = user.id;
+
         return {
             user,
         };
--- src/types.ts
+++ src/types.ts
@@ -1,5 +1,9 @@
 import { Connection, EntityManager, IDatabaseDriver } from "@mikro-orm/core";
+import { Request, Response } from "express";
 
-export type MyContext {
-    em:  EntityManager<IDatabaseDriver<Connection>>
+
+export type MyContext = {
+    em:  EntityManager<IDatabaseDriver<Connection>>;
+    req: Request & {session?: Express.Session };
+    res: Response;
 }
